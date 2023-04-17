d0d415e4ba9f7974dbf9fe0f04939a12a3450289
Ingrid G <ing.gape18@gmail.com>
2023-04-17 08:58:03 -0500

    problemas al exportar modulos

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template></template>
+
+<script setup>
+import TravelMap from "./components/TravelMap.vue";
+</script>
+
+<style scoped></style>
---
+++ src/components/TravelMap.vue
@@ -0,0 +1,27 @@
+<template>
+  <GoogleMapLoader :mapConfig="mapConfig" apiKey="yourApiKey">
+    <template slot-scope="{ google, map }">
+      {{ map }}
+      {{ google }}
+    </template>
+  </GoogleMapLoader>
+</template>
+
+<script setup>
+import GoogleMapLoader from "./GoogleMapLoader.vue";
+//import { mapSettings } from "./constants/mapSettings";
+
+export default {
+  components: {
+    GoogleMapLoader,
+  },
+  computed: {
+    mapConfig() {
+      return {
+        ...mapSettings,
+        center: { lat: 0, lng: 0 },
+      };
+    },
+  },
+};
+</script>
